110ddbab19ef7df3dbc8678ba54651e6a7f84b94
hojin <onexy91@naver.com>
2021-03-24 17:52:39 +0900

    propTypes Study

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React from "react"
-
-function Food({ name, image }) {
-  //console.log(props.name);
-  return <div>
-    <h1>I like {name}</h1>
-    <img src={image}></img>
-  </div>
-}
+import PropTypes from "prop-types"
 
 const foodList = [
   {
+    id: 1,
     name: "doncasu",
-    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5
   },
   {
+    id: 2,
     name: "kimbap",
-    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.9
   },
   {
+    id: 3,
     name: "kimchi",
-    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    image: "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.7
   }
 ]
+
+function Food({ name, image, rating }) {
+  //console.log(props.name);
+  return <div>
+    <h1>I like {name}</h1>
+    <h4>{rating}/5.0</h4>
+    <img src={image} alt={name}></img>
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div>
-     {foodList.map(food => <Food name={food.name} image={food.image}/> )}
+     {foodList.map(food => <Food key={food.id} name={food.name} image={food.image} rating={food.rating}/> )}
     </div>
   );
 }
